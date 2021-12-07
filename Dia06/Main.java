2f58e05ba7d182c08f4610f4431b10a7b3d62ef4
Idir Carlos Aliane Crespo <ic.aliane@alumnos.upm.es>
2021-12-07 15:52:18 +0100

    Linear solution for the first problem

--- Dia06/Main.java
+++ Dia06/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static final int MAX_DAYS = 80;
+
     private static List<Integer> toIntegerList(String [] numbers){
         List <Integer> list = new LinkedList<>();
         for (int i = 0; i < numbers.length; i++){
@@ -12,35 +13,35 @@ public class Main {
         return list;
     }
 
-    public static void first(){
-        Scanner sc = new Scanner(System.in);
-        String [] parsed = sc.nextLine().split(",");
-        List<Integer> list = toIntegerList(parsed);
-        //System.out.println(list);
-        int day = 0;
-        while (day < MAX_DAYS){
-            System.out.printf("Day %d\n",day);
-            for (int i = 0; i < list.size(); i++){
-                if (list.get(i) == 0){
-                    list.add(9);
-                    list.set(i, 7);
-                }
-                list.set(i, list.get(i) - 1);
-                
-            }
-            //System.out.printf("Day %d: %s\n",day,list);
-            day++;
-        }
-        
-        System.out.println(list.size());
+    public static int first(List<Integer> lanterfishes){
+        return firstAux(lanterfishes);
     }
 
-    public static void second(){
+    public static int firstAux(List<Integer> lanterfishes){
+        int ret = lanterfishes.size();
+        for(int i = 0; i < lanterfishes.size(); i++){
+            ret = ret + howManyChildsFrom(lanterfishes.get(i), 0, 0, 0);
+        }
+        System.out.println(ret);
+        return 0;
+    }
 
+    public static int howManyChildsFrom(Integer initialState, Integer actualDay, Integer total, Integer id){
+        int cycles = initialState - MAX_DAYS + actualDay;
+        while (cycles < 0 && actualDay <= MAX_DAYS){
+            cycles = cycles + 7;
+            actualDay = actualDay + 7;
+            total = howManyChildsFrom(8 - (6 - initialState), actualDay, total+1, id+1); //Cycle (8) - (6 days - initialState)
+        }
+        return total;
     }
     
     public static void main(String [] args){
-        first();
-        second();
+        Scanner sc = new Scanner(System.in);
+        String [] parsed = sc.nextLine().split(",");
+        List<Integer> list = toIntegerList(parsed);
+        first(list);
+        //second(list);
+        sc.close();
     }
 }
